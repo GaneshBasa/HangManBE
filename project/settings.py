a80bc684c9444d2f689ec17f0f20a2c2ee502172
Ganesh Basa <basaganesh1@gmail.com>
2024-06-20 15:20:20 +0530

    REVERT

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mlz%ljxx652!!&&lq89%_njwd*d5sedi+=)gf#(xy-g4pi2z8)'
 
 # NOTE: SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', '.now.sh']
 
